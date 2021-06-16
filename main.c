08f4b24e42dacb59b8464b15ea525e06b71eb906
nift4 <nift4@protonmail.com>
2021-06-16 18:52:24 +0200

    Add WIP split
    Change-Id: I33803ee3e5314683984ec0a56088061ab140fd4c

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 char* readfullfile(char* name) {	
 	FILE *f = fopen(name, "rb");
@@ -19,6 +20,44 @@ char* readfullfile(char* name) {
 	return string;
 }
 
+struct split_string {
+	int len;
+	char** str;
+};
+typedef struct split_string splitstr;
+// hand written
+splitstr* split(char* string, char* delimiter) {
+	int targetsize = 0;
+	splitstr* ret = malloc(sizeof(splitstr));
+	if (ret == NULL)
+		return NULL;
+	ret->str = NULL;
+	char* pos;
+	char* oldpos = string;
+	int newsize;
+	int dlen = strlen(delimiter);
+	do {
+		pos = strstr(oldpos, delimiter);
+		if (pos) {
+			newsize = pos - oldpos;
+		} else {
+			newsize = strlen(oldpos);
+		}
+		char* newstr = malloc(sizeof(char) * (newsize + 1));
+		strncpy(newstr, oldpos, newsize);
+		newstr[newsize] = '\0';
+		oldpos = pos + dlen;
+		printf("Found %s\n", newstr);
+		ret->str = realloc(ret->str, (targetsize+1) * sizeof(char*));
+		ret->str[targetsize++] = newstr;
+		//ret->len++;
+	} while (pos != NULL);
+	for (int i = 0; i < ret->len; i++) {
+		printf("Element %d: %s", i, ret->str[i]);
+	}
+	return ret;
+}
+
 int main(int argc, char** argv) {
 	if (argc != 2)
 		return 1;
@@ -26,5 +65,6 @@ int main(int argc, char** argv) {
 	if (contents == NULL)
 		return 2;
 	printf("%s", contents);
+	split(contents, "a");
 	return 0;
 }
