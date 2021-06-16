c7081aee21d02a59da4bb1b4aa36961d4d0778da
nift4 <nift4@protonmail.com>
2021-06-16 17:04:54 +0200

    Add readfullfile()
    Change-Id: I0ceb288155362b3f6b25d1e6ba5ec9b861b3b520

--- main.c
+++ main.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+char* readfullfile(char* name) {	
+	FILE *f = fopen(name, "rb");
+	if (f == NULL)
+		return NULL;
+	fseek(f, 0, SEEK_END);
+	long fsize = ftell(f);
+	rewind(f);
+	char *string = malloc(fsize + 1);
+	if (string == NULL) {
+		fclose(f);
+		return NULL;
+	}
+	fread(string, 1, fsize, f);
+	fclose(f);
+	string[fsize] = 0;
+	return string;
+}
 
 int main(int argc, char** argv) {
-	printf("buildthing");
+	if (argc != 2)
+		return 1;
+	char* contents = readfullfile(argv[1]);
+	if (contents == NULL)
+		return 2;
+	printf("%s", contents);
 	return 0;
 }
