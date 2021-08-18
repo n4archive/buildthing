03fdda117326717bba68127a9961a2364bb37a85
nift4 <nift4@protonmail.com>
2021-08-18 18:30:37 +0200

    [parser] seperate names from inside API
    Change-Id: I63033328f52f6dae9bae393c3c677fe26564d8f4

--- parser/parser.c
+++ parser/parser.c
@@ -18,36 +18,36 @@ inline char *ef(int UNUSED(i)) {
   return err;
 }
 
-void skip_punc(token_stream *input, char punc) {
+void parser_skip_punc(token_stream *input, char punc) {
   token *t = tkstr_next(input);
   if (!(t->type == TKSTR_PUNC && t->raw[0] == punc && t->raw[1] == '\0'))
     tkstr_fail(input, ef(sprintf(err, "Unexcepted input, I wanted %c", punc)));
   destroy_token(t);
 }
 
-bool ensure_punc(token_stream *input, char punc) {
+bool parser_ensure_punc(token_stream *input, char punc) {
   token *t = tkstr_peek(input);
   return t && t->type == TKSTR_PUNC && t->raw[0] == punc && t->raw[1] == '\0';
 }
 
-void skip_op(token_stream *input, char *op) {
+void parser_skip_op(token_stream *input, char *op) {
   token *t = tkstr_next(input);
   if (!(t->type == TKSTR_OP && strcmp(t->raw, op) == 0))
     tkstr_fail(input, ef(sprintf(err, "Unexcepted input, I wanted %s", op)));
   destroy_token(t);
 }
 
-bool ensure_op(token_stream *input, char *op) {
+bool parser_ensure_op(token_stream *input, char *op) {
   token *t = tkstr_peek(input);
   return t && t->type == TKSTR_OP && strcmp(op, t->raw) == 0;
 }
 
-bool ensure_kw(token_stream *input, char *kw) {
+bool parser_ensure_kw(token_stream *input, char *kw) {
   token *t = tkstr_peek(input);
   return t && t->type == TKSTR_KEYWORD && strcmp(kw, t->raw) == 0;
 }
 
-void skip_kw(token_stream *input, char *kw) {
+void parser_skip_kw(token_stream *input, char *kw) {
   token *t = tkstr_next(input);
   if (!(strcmp(kw, t->raw) == 0 && t->type == TKSTR_KEYWORD))
     tkstr_fail(input, ef(sprintf(err, "Unexcepted input, I wanted %s", kw)));
@@ -60,15 +60,15 @@ int parse_delimited(token_stream *input, char start, char stop, char delimiter,
   ast_node **a = malloc(sizeof(ast_node *) * size);
   int count = 0;
   bool first = true;
-  skip_punc(input, start);
+  parser_skip_punc(input, start);
   while (!input->eof) {
-    if (ensure_punc(input, stop))
+    if (parser_ensure_punc(input, stop))
       break;
     if (first)
       first = false;
     else
-      skip_punc(input, delimiter);
-    if (ensure_punc(input, stop))
+      parser_skip_punc(input, delimiter);
+    if (parser_ensure_punc(input, stop))
       break; // the last separator can be missing
     a[count++] = parser(input);
     if (count == size) {
@@ -76,7 +76,7 @@ int parse_delimited(token_stream *input, char start, char stop, char delimiter,
       a = realloc(a, size * sizeof(ast_node *));
     }
   }
-  skip_punc(input, stop);
+  parser_skip_punc(input, stop);
   *ret = a;
   return count;
 }
@@ -91,7 +91,7 @@ ast_node *parse_varname(token_stream *input) {
   BOX(AST_VAR, tret)
 }
 
-int prec(char *op) {
+int _parser_prec(char *op) {
 #define c(a, b)                                                                \
   if (strcmp(op, a) == 0)                                                      \
     return b;
@@ -111,13 +111,13 @@ ast_node *parse_bool(token_stream *input) {
 }
 
 ast_node *parse_if(token_stream *input) {
-  skip_kw(input, "if");
+  parser_skip_kw(input, "if");
   ast_node *cond = parse_expression(input);
-  if (!ensure_punc(input, '{'))
-    skip_kw(input, "then");
+  if (!parser_ensure_punc(input, '{'))
+    parser_skip_kw(input, "then");
   ast_node *then = parse_expression(input);
   ast_if *ret = malloc(sizeof(ast_if));
-  if (ensure_kw(input, "else")) {
+  if (parser_ensure_kw(input, "else")) {
     destroy_token(tkstr_next(input));
     ret->elsee = parse_expression(input);
   } else {
@@ -136,22 +136,24 @@ ast_node *parse_call(token_stream *input, ast_node *name) {
   BOX(AST_CALL, ret)
 }
 
-ast_node *maybe_call(token_stream *input, ast_node *(*func)(token_stream *)) {
+ast_node *parser_maybe_call(token_stream *input,
+                            ast_node *(*func)(token_stream *)) {
   ast_node *expr = func(input);
-  if (ensure_punc(input, '('))
+  if (parser_ensure_punc(input, '('))
     return parse_call(input, expr);
   return expr;
 }
 
-ast_node *maybe_binary(token_stream *input, ast_node *left, int my_prec) {
+ast_node *parser_maybe_binary(token_stream *input, ast_node *left,
+                              int my_prec) {
   token *tok = tkstr_peek(input);
   if (tok && tok->type == TKSTR_OP) {
-    int his_prec = prec(tok->raw);
+    int his_prec = _parser_prec(tok->raw);
     if (his_prec > my_prec) {
-      bool is_assign = ensure_op(input, "=");
+      bool is_assign = parser_ensure_op(input, "=");
       destroy_token(tkstr_next(input)); // consume the token we peeked
       ast_node *rett = malloc(sizeof(ast_node));
-      ast_node *right = maybe_binary(input, parse_atom(input), his_prec);
+      ast_node *right = parser_maybe_binary(input, parse_atom(input), his_prec);
       if (is_assign) {
         rett->type = AST_ASSIGN;
         ast_assign *ret = malloc(sizeof(ast_assign));
@@ -166,35 +168,35 @@ ast_node *maybe_binary(token_stream *input, ast_node *left, int my_prec) {
         ret->right = right;
         rett->content = ret;
       }
-      return maybe_binary(input, rett, my_prec);
+      return parser_maybe_binary(input, rett, my_prec);
     }
   }
   return left;
 }
 
 ast_node *_real_parse_atom(token_stream *input) {
-  if (ensure_punc(input, '(')) {
+  if (parser_ensure_punc(input, '(')) {
     destroy_token(tkstr_next(input));
     ast_node *exp = parse_expression(input);
-    skip_punc(input, ')');
+    parser_skip_punc(input, ')');
     return exp;
   }
 
-  if (ensure_op(input, "!")) {
-    skip_op(input, "!");
+  if (parser_ensure_op(input, "!")) {
+    parser_skip_op(input, "!");
     ast_unary *ret = malloc(sizeof(ast_unary));
     ret->operatorr = "!";
     ret->contents = parse_atom(input);
     BOX(AST_UNARY, ret)
   }
 
-  if (ensure_punc(input, '{'))
+  if (parser_ensure_punc(input, '{'))
     return parse_prog(input);
-  if (ensure_kw(input, "if"))
+  if (parser_ensure_kw(input, "if"))
     return parse_if(input);
-  if (ensure_kw(input, "true") || ensure_kw(input, "false"))
+  if (parser_ensure_kw(input, "true") || parser_ensure_kw(input, "false"))
     return parse_bool(input);
-  if (ensure_kw(input, "function")) {
+  if (parser_ensure_kw(input, "function")) {
     destroy_token(tkstr_next(input));
     return parse_func(input);
   }
@@ -219,15 +221,15 @@ ast_node *_real_parse_atom(token_stream *input) {
 }
 
 ast_node *_real_parse_expression(token_stream *input) {
-  return maybe_binary(input, parse_atom(input), 0);
+  return parser_maybe_binary(input, parse_atom(input), 0);
 }
 
 ast_node *parse_atom(token_stream *input) {
-  return maybe_call(input, _real_parse_atom);
+  return parser_maybe_call(input, _real_parse_atom);
 }
 
 ast_node *parse_expression(token_stream *input) {
-  return maybe_call(input, _real_parse_expression);
+  return parser_maybe_call(input, _real_parse_expression);
 }
 
 ast_node *parse_func(token_stream *input) {
@@ -247,7 +249,7 @@ ast_node *parse_prog(token_stream *input) {
     while (!input->eof) {
       prog[count++] = parse_expression(input);
       if (!input->eof)
-        skip_punc(input, ';');
+        parser_skip_punc(input, ';');
       if (count == size) {
         size += 5;
         prog = realloc(prog, size * sizeof(ast_prog *));
